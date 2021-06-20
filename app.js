1f4c171d88d0c867739e59f0a244d300481d3fac
LoanaRosi <loarosi24@gmail.com>
2021-06-20 02:26:20 -0300

    arañas

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3030;
+const path = require('path')
+
+app.use(express.static('public'));
+
+
+app.get('/',(req,res) => res.sendFile(path.join(__dirname, 'views', 'home.html')));
+app.get('/spiders', (req, res) => res.sendFile(path.join(__dirname, 'views', 'spiders.html')));
+app.get('/adicional', (req, res) => res.sendFile(path.join(__dirname, 'views', 'adicional.html')));
+
+app.listen(port,() => console.log('Servidor corriendo en http://localhost:' + port))
